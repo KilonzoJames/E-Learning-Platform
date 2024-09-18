9d7ce50a94a39aa408090c71395a18ed8d1b0029
KilonzoJames <jay.kilonzo@gmail.com>
2024-09-18 19:34:41 +0300

    Added proxy

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'https://e-learning-platform-1-10z1.onrender.com'
+    }
+  },
   plugins: [react()],
 })
